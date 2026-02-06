ffcbe387bf7842885d94b8e956a84442e010d737
Pavlo Kilko <pavel.kilko@gmail.com>
2026-02-06 23:12:23 +0200

    feat: add DoclingPageLoadNode, IndexRunner

--- src/merlin/rag/index/nodes/__init__.py
+++ src/merlin/rag/index/nodes/__init__.py
@@ -1,5 +1,6 @@
 from .index_node import IndexNode
 from .docling_load_split_node import DoclingLoadSplitNode
+from .docling_page_load_node import DoclingPageLoadNode
 from .pypdf_load_node import PyPDFLoadNode
 from .split_node import SplitNode
 from .embed_store_node import EmbedStoreNode
@@ -7,6 +8,7 @@ from .embed_store_node import EmbedStoreNode
 __all__ = [
     "IndexNode",
     "DoclingLoadSplitNode",
+    "DoclingPageLoadNode",
     "PyPDFLoadNode",
     "SplitNode",
     "EmbedStoreNode"
