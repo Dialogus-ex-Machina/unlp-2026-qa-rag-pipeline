e18b9fe233fb0751ca4a43197ab2c52eaf49f0f2
Pavlo Kilko <pavel.kilko@gmail.com>
2026-02-13 20:25:34 +0200

    add improved docling load-split node

--- src/merlin/rag/index/nodes/__init__.py
+++ src/merlin/rag/index/nodes/__init__.py
@@ -1,6 +1,7 @@
 from .index_node import IndexNode
 from .docling_load_split_node import DoclingLoadSplitNode
 from .docling_page_load_node import DoclingPageLoadNode
+from .docling_rag_load_split_node import DoclingRagLoadSplitNode
 from .pypdf_load_node import PyPDFLoadNode
 from .pymupdf_load_node import PyMuPDFLoadNode
 from .split_node import SplitNode
@@ -10,6 +11,7 @@ __all__ = [
     "IndexNode",
     "DoclingLoadSplitNode",
     "DoclingPageLoadNode",
+    "DoclingRagLoadSplitNode",
     "PyPDFLoadNode",
     "PyMuPDFLoadNode",
     "SplitNode",
