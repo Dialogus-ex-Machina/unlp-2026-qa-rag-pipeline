ad3faa1d603b34a272fb3b24daab56485e815cc3
Kolya Nosenko <nikolay.nosenko@knu.ua>
2026-02-16 15:34:18 +0200

    feat: Add reordering to top_k_relevant

--- src/unlp_2026_submission/workflow/nodes/top_k_relevant_document_augmentation_node.py
+++ src/unlp_2026_submission/workflow/nodes/top_k_relevant_document_augmentation_node.py
@@ -1,10 +1,13 @@
+from typing import Literal
+
+from unlp_2026_submission.entities import RelevantDocument
 from unlp_2026_submission.workflow.nodes.base_node import BaseNode
 from unlp_2026_submission.workflow.state import QAWorkflowState
 
 class TopKRelevantDocumentAugmentation(BaseNode):
     _top_k: int
 
-    def __init__(self, top_k: int = 3):
+    def __init__(self, top_k: Literal[1, 3, 5] = 3):
         self._top_k = top_k
 
     def __call__(self, state: QAWorkflowState):
@@ -27,16 +30,10 @@ class TopKRelevantDocumentAugmentation(BaseNode):
                 'relevant_document_page_num': -1,
             }
 
-        top_k_relevant = relevant_documents[0:self._top_k]
+        relevant_context = self._create_relevant_context(relevant_documents)
 
-        # separator = "\n---------------------\n"
-        separator = "\n\n"
-        relevant_context = separator.join(
-            page.text for page in top_k_relevant
-        )
-
-        relevant_document_id = top_k_relevant[0].document_id
-        relevant_document_page_num = top_k_relevant[0].page_number
+        relevant_document_id = relevant_documents[0].document_id
+        relevant_document_page_num = relevant_documents[0].page_number
 
         print('Retrieved doc_id:', relevant_document_id)
         print('Retrieved page_num:', relevant_document_page_num)
@@ -48,3 +45,25 @@ class TopKRelevantDocumentAugmentation(BaseNode):
             'relevant_document_id': relevant_document_id,
             'relevant_document_page_num': relevant_document_page_num,
         }
+
+    def _create_relevant_context(self, relevant_documents: list[RelevantDocument]) -> str:
+        k = min(self._top_k, len(relevant_documents))
+        docs = relevant_documents[:k]
+
+        top_k_relevant = self._reorder_docs(docs)
+
+        separator = "\n\n"
+        relevant_context = separator.join(doc.text for doc in top_k_relevant)
+
+        return relevant_context
+
+    def _reorder_docs(self, docs: list[RelevantDocument]) -> list[RelevantDocument]:
+        n = len(docs)
+        # evens: 0,2,4,...
+        evens = list(range(0, n, 2))
+        # odds: 1,3,5,... then reverse -> ...,3,1
+        odds = list(range(1, n, 2))[::-1]
+        order = evens + odds
+        reordered = [docs[i] for i in order]
+
+        return reordered
