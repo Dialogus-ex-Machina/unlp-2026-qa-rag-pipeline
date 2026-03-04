a1a42058dae2d6d197567a5fc4c14bc5b8676094
Kolya Nosenko <nikolay.nosenko@knu.ua>
2026-03-04 17:17:03 +0200

    feat: Add runs/splitting experiments implementation

---
+++ scripts/runs/splitting/sentence_800_0_splitting.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from merlin.rag.index import IndexState, IndexRunner
+from merlin.rag.index.nodes import TxtLoadNode
+from merlin.rag.index.nodes.hybrid_embed_store_node import HybridEmbedStoreNode
+from merlin.rag.index.nodes.page_aware_document_split_node import PageAwareDocumentSplitNode
+from merlin.rag.index.splitters import SentenceSplitter
+from unlp_2026_submission.config import Config
+from unlp_2026_submission.embeddings import EmbeddingsModelFactory
+
+def get_txt_filepaths(documents_dir: str = "../documents") -> list[str]:
+    p = Path(documents_dir)
+    # recursive; use p.glob("*.pdf") if you only want top-level
+    return sorted(str(fp) for fp in p.rglob("*.txt") if fp.is_file())
+
+"""Index pipeline configuration:
+    1) Load: TxtLoadNode
+    2) Split: SentenceSplitter(chunk_size=800, chunk_overlap=0)
+    3) HybridEmbedStoreNode()
+"""
+
+def main():
+    config = Config()
+
+    embeddings_model = EmbeddingsModelFactory.create(config).get_embeddings_model()
+
+    nodes = [
+        TxtLoadNode(),
+        PageAwareDocumentSplitNode(
+            splitter=SentenceSplitter(
+                chunk_size=800,
+                chunk_overlap=0,
+            )
+        ),
+        HybridEmbedStoreNode(
+            embeddings=embeddings_model,
+            batch_size=1,
+            collection_name=config.vector_store['collection_name'],
+        )
+    ]
+
+    index_runner = IndexRunner(nodes)
+
+    filepaths = get_txt_filepaths(
+        config.data_root_dir
+    )
+
+    initial_state: IndexState = {
+        "filepaths": filepaths,
+        "vector_store_path": config.vector_store['path'],
+    }
+
+    final_state = index_runner.run(initial_state)
+
+    print('Final state:', final_state)
+
+if __name__ == "__main__":
+    main()
